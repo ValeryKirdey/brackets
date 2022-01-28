ffa846d60d3e178ec48ad72dea4b3fdfed1afaca
Valery <v.a.l.e.r.a95@mail.ru>
2022-01-28 14:38:29 +0300

    feat: add brackets decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    while (true) {
+        let arr = str;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let parentheses = bracketsConfig[i][0] + bracketsConfig[i][1];
+            if (arr.indexOf(parentheses) != -1) {
+                arr = arr.replace(parentheses, "");
+            }
+        }
+        if (arr == str) {
+            break;
+        }
+        str = arr;
+    }
+    return str == "";
 }
